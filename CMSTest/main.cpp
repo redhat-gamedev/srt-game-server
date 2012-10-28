62d12519ef3a597186c2b70002defaa8a6cc6de0
Roddie Kieley <roddie@fusesource.com>
2012-10-28 14:48:44 -0230

    Initial Commit

---
+++ CMSTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CMSTest
+//
+//  Created by Roddie Kieley on 12-10-28.
+//  Copyright (c) 2012 Roddie Kieley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
